929ccdfa8b2d821a95ccfae0f4a55815555c46bd
Fahmi <fislam@enstoa.com>
2023-05-15 22:39:02 -0400

    Initial testing

--- streamlit_app.py
+++ streamlit_app.py
@@ -45,7 +45,7 @@ st.title("st.file_uploader")
 st.subheader("Input CSV / Excel file")
 uploaded_file = st.file_uploader("Choose a file")
 
-if upoaded_file is not None:
+if uploaded_file is not None:
     df = pd.read_csv(uploaded_file)
     st.subheader("Dataframe")
     st.write(df)
