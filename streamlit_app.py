1640370a545a1b206b1eb470da786cfbe6d95fa1
Fahmi <fislam@enstoa.com>
2023-05-15 23:52:32 -0400

    initial testing

--- streamlit_app.py
+++ streamlit_app.py
@@ -205,3 +205,104 @@ with st.expander("Tickets Per Month"):
 
     # plt.savefig('tickets_per_month.png', dpi=300, bbox_inches='tight')
     st.pyplot(fig=fig)
+
+df3 = (
+    df.query("ticket_status == 'Closed' and year_opened >= 2019")
+    .groupby(by=["year_opened", "month_opened"])
+    .days_active.agg(np.mean)
+    .reset_index(name="average_days_active")
+)
+
+df3["rounded_days_active"] = np.ceil(
+    df3["average_days_active"].dt.total_seconds() / (24 * 3600)
+).astype(int)
+
+with st.expander("Average days to close tickets"):
+    fig, ax = plt.subplots(figsize=(13.6, 8))
+
+    bplot = sns.barplot(
+        data=df3,
+        x="month_opened",
+        y="rounded_days_active",
+        hue="year_opened",
+        palette=custom_palette,
+        ax=ax,
+        errorbar=None,
+        alpha=0.15,
+    )
+
+    ax.set_xlim(-0.5, 11.5)
+    ax.set_ylim(0, ax.get_ylim()[1])
+
+    ax.legend_.remove()
+
+    ax.set_xticklabels([calendar.month_name[i] for i in range(1, 13)])
+    # ax.set_xlabel("Month of Year", fontsize=14, labelpad=12)
+    ax.set_xlabel("")
+    ax.set_ylabel("Mean Days to Ticket Closure", fontsize=12, labelpad=15)
+    ax.tick_params(axis="both", length=0)
+    ax.tick_params(axis="y", pad=28)
+
+    ax2 = fig.add_axes([0.1, 0.11, 0.76, 0.77])  # left, bottom, width, height
+    ax2.patch.set_alpha(0)
+
+    lplot = sns.lineplot(
+        data=df3,
+        x="month_opened",
+        y="rounded_days_active",
+        hue="year_opened",
+        palette=custom_palette[:5],
+        ax=ax2,
+        errorbar=None,
+        alpha=1,
+        legend=True,
+    )
+
+    ax2.set_xlim(0.125, 12)
+    ax2.set_ylim(0, ax.get_ylim()[1])
+
+    handles, labels = ax2.get_legend_handles_labels()
+    handles = handles[::-1]
+    labels = labels[::-1]
+    ax2.legend(
+        handles,
+        labels,
+        # loc="best",
+        facecolor="white",
+        framealpha=1,
+        bbox_to_anchor=(0.9, 0.648),
+    )
+    legend.get_title().set_fontsize(12)
+    legend.get_frame().set_linewidth(0.25)
+
+    ax2.set_xticklabels([])
+    ax2.set_yticklabels([])
+    ax2.set_xlabel("")
+    ax2.set_ylabel("")
+    ax2.tick_params(axis="both", length=0)
+
+    ax2.axvline(x=4.55, linestyle="--", color="red", ymin=0, ymax=0.95)
+
+    ax2.text(
+        1.93,
+        plt.ylim()[1] * 0.865,
+        "End of full month data for 2023",
+        color="r",
+        ha="left",
+        rotation=0,
+    )
+    ax2.grid(color="k", linestyle="-", axis="y", alpha=0.1)
+
+    plt.suptitle(
+        "The average number of days to close a ticket has been on a general decline since mid-2022",
+        fontsize=14,
+        ha="left",
+        va="top",
+        x=0.1,
+        y=0.9,
+    )
+
+    sns.despine(bottom=True, left=True)
+
+    # plt.savefig('average_days_to_close.png', dpi=300, bbox_inches='tight')
+    st.pyplot(fig=fig)
