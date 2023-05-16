5407463b12b025c227000cfa860d23b13c271e9d
Fahmi <fislam@enstoa.com>
2023-05-16 00:11:38 -0400

    initial testing

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,12 +14,7 @@ st.set_page_config(layout="wide")
 """
 # Enstoa COE KPI Report
 
-Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:
-
-If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
-forums](https://discuss.streamlit.io).
-
-In the meantime, below is an example of what you can do with just a few lines of code:
+Questions / comments / concerns / requests: [fislam@enstoa.com](mailto:fislam@enstoa.com)
 """
 
 
