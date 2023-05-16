44187e3fdbefb4b6db99a3606d8302657fe6baa8
Fahmi <fislam@enstoa.com>
2023-05-15 23:47:01 -0400

    Initial Testing

--- streamlit_app.py
+++ streamlit_app.py
@@ -112,98 +112,96 @@ custom_palette = sns.color_palette(
     ["#264653", "#2A9D8F", "#E9C46A", "#F4A261", "#E76F51", "#E97C61"]
 )
 
-# with st.expander('title')
+with st.expander("Tickets Per Month"):
+    # Fig 1: Tickets per month
+    fig, ax = plt.subplots(figsize=(13.6, 8))
 
-# Fig 1: Tickets per month
-fig, ax = plt.subplots(figsize=(13.6, 8))
+    bplot = sns.barplot(
+        data=df_monthly_grouped.query("year_opened >= 2019"),
+        x="month_opened",
+        y="ticket_count",
+        hue="year_opened",
+        palette=custom_palette,
+        ax=ax,
+        errorbar=None,
+        alpha=0.2,
+    )
 
-bplot = sns.barplot(
-    data=df_monthly_grouped.query("year_opened >= 2019"),
-    x="month_opened",
-    y="ticket_count",
-    hue="year_opened",
-    palette=custom_palette,
-    ax=ax,
-    errorbar=None,
-    alpha=0.2,
-)
+    ax.set_xlim(-0.5, 11.5)
+    ax.set_ylim(0, ax.get_ylim()[1])
 
-ax.set_xlim(-0.5, 11.5)
-ax.set_ylim(0, ax.get_ylim()[1])
+    ax.legend_.remove()
 
-ax.legend_.remove()
+    ax.set_xticklabels([calendar.month_name[i] for i in range(1, 13)])
+    # ax.set_xlabel("Month of Year", fontsize=14, labelpad=12)
+    ax.set_xlabel("")
+    ax.set_ylabel("Tickets Opened", fontsize=12, labelpad=15)
+    ax.tick_params(axis="both", length=0)
+    ax.tick_params(axis="y", pad=10)
 
-ax.set_xticklabels([calendar.month_name[i] for i in range(1, 13)])
-# ax.set_xlabel("Month of Year", fontsize=14, labelpad=12)
-ax.set_xlabel("")
-ax.set_ylabel("Tickets Opened", fontsize=12, labelpad=15)
-ax.tick_params(axis="both", length=0)
-ax.tick_params(axis="y", pad=10)
+    ax2 = fig.add_axes([0.119, 0.11, 0.76, 0.77])  # left, bottom, width, height
+    ax2.patch.set_alpha(0)
 
-ax2 = fig.add_axes([0.119, 0.11, 0.76, 0.77])  # left, bottom, width, height
-ax2.patch.set_alpha(0)
+    lplot = sns.lineplot(
+        data=df_monthly_grouped.query(
+            "(year_opened >= 2019 & year_opened < 2023) | (year_opened == 2023 & month_opened < 5)"
+        ),
+        x="month_opened",
+        y="ticket_count",
+        hue="year_opened",
+        palette=custom_palette[:5],
+        ax=ax2,
+        errorbar=None,
+        alpha=1,
+        legend=True,
+    )
 
-lplot = sns.lineplot(
-    data=df_monthly_grouped.query(
-        "(year_opened >= 2019 & year_opened < 2023) | (year_opened == 2023 & month_opened < 5)"
-    ),
-    x="month_opened",
-    y="ticket_count",
-    hue="year_opened",
-    palette=custom_palette[:5],
-    ax=ax2,
-    errorbar=None,
-    alpha=1,
-    legend=True,
-)
+    ax2.set_xlim(0.125, 12)
+    ax2.set_ylim(0, ax.get_ylim()[1])
 
-ax2.set_xlim(0.125, 12)
-ax2.set_ylim(0, ax.get_ylim()[1])
+    handles, labels = ax2.get_legend_handles_labels()
+    handles = handles[::-1]
+    labels = labels[::-1]
+    legend = ax2.legend(
+        handles,
+        labels,
+        # loc="best",
+        facecolor="white",
+        framealpha=1,
+        bbox_to_anchor=(0.9, 0.728),
+    )
+    # legend.set_title("Year")
+    legend.get_title().set_fontsize(12)
+    legend.get_frame().set_linewidth(0.25)
 
-handles, labels = ax2.get_legend_handles_labels()
-handles = handles[::-1]
-labels = labels[::-1]
-legend = ax2.legend(
-    handles,
-    labels,
-    # loc="best",
-    facecolor="white",
-    framealpha=1,
-    bbox_to_anchor=(0.9, 0.728),
-)
-# legend.set_title("Year")
-legend.get_title().set_fontsize(12)
-legend.get_frame().set_linewidth(0.25)
+    ax2.set_xticklabels([])
+    ax2.set_yticklabels([])
+    ax2.set_xlabel("")
+    ax2.set_ylabel("")
+    ax2.tick_params(axis="both", length=0)
 
-ax2.set_xticklabels([])
-ax2.set_yticklabels([])
-ax2.set_xlabel("")
-ax2.set_ylabel("")
-ax2.tick_params(axis="both", length=0)
+    ax2.axvline(x=4.25, linestyle="--", color="red", ymin=0, ymax=0.95)
 
-ax2.axvline(x=4.25, linestyle="--", color="red", ymin=0, ymax=0.95)
+    ax2.text(
+        4.37,
+        plt.ylim()[1] * 0.82,
+        "End of full month data for 2023",
+        color="r",
+        ha="left",
+        rotation=0,
+    )
+    ax2.grid(color="k", linestyle="-", axis="y", alpha=0.1)
 
-ax2.text(
-    4.37,
-    plt.ylim()[1] * 0.82,
-    "End of full month data for 2023",
-    color="r",
-    ha="left",
-    rotation=0,
-)
-ax2.grid(color="k", linestyle="-", axis="y", alpha=0.1)
+    plt.suptitle(
+        "The number of tickets opened per month has increased significantly YoY for 2022 and the start of 2023",
+        fontsize=14,
+        ha="left",
+        va="top",
+        x=0.12,
+        y=0.93,
+    )
 
+    sns.despine(bottom=True, left=True)
 
-plt.suptitle(
-    "The number of tickets opened per month has increased significantly YoY for 2022 and the start of 2023",
-    fontsize=14,
-    ha="left",
-    va="top",
-    x=0.12,
-    y=0.93,
-)
-
-sns.despine(bottom=True, left=True)
-
-# plt.savefig('tickets_per_month.png', dpi=300, bbox_inches='tight')
-st.pyplot()
+    # plt.savefig('tickets_per_month.png', dpi=300, bbox_inches='tight')
+    st.pyplot(fig=fig)
